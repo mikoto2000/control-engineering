b29745d693da812ec494f9c22e14d0d5e346dc99
mikoto2000 <mikoto2000@gmail.com>
2019-02-07 03:03:01 +0900

    Project setting.

--- source/conf.py
+++ source/conf.py
@@ -77,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -182,3 +182,5 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+math_number_all = True
